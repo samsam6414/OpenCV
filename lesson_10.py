08c7b8a83fa0c1d7f08f03bde83ee13a7a07ad90
samsam6414 <99627872+samsam6414@users.noreply.github.com>
2022-03-10 15:45:53 +0800

    new

---
+++ lesson_10.py
@@ -0,0 +1,15 @@
+import cv2
+from matplotlib.pyplot import gray
+
+img = cv2.imread('image_person.jpg')
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+faceCascade = cv2.CascadeClassifier('face_detect.xml')
+faceRect = faceCascade.detectMultiScale(gray,(1.1), 2)
+print(len(faceRect))
+
+for (x, y, w, h) in faceRect:
+    cv2.rectangle(img , (x, y), (x+w, y+h), (0,255,0), 2)
+
+cv2.imshow('img',img)
+cv2.waitKey(0)
+
